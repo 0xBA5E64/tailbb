3fb771a07d32e3405aaab2f15686e369ecb2b04c
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2025-08-06 22:37:45 +0200

    feat: Finish `dev-db.sh` convenience script with idle container starting functionality.

--- dev-db.sh
+++ dev-db.sh
@@ -1,8 +1,15 @@
 
 if docker inspect taildb >& /dev/null; then
     echo Container exists
+    if docker inspect taildb | grep '"Status": "running"' >& /dev/null; then
+        echo ...and running. Exiting.
+    else
+        echo ...but not running. Starting container...
+        docker start taildb
+        echo ...container started. Exiting.
+    fi
 else
-    echo Container does not exist, creating...
+    echo Container does not exist. Creating...
 
     docker run -tid --name taildb -p 5100:5432/tcp -v .db-data:/var/lib/postgresql/data -e POSTGRES_USER=taildb -e POSTGRES_PASSWORD=taildb postgres:17
     sqlx migrate run
